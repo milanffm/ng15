7384ea922db140dcd80742eb8495eed5ca157769
Milan Peters <m.peters@graphicon.de>
2023-02-17 23:48:37 +0100

    add lazy loading

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,25 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import { TodosPageComponent } from './app/todos-page/todos-page.component';
-const routes: Routes = [{
-  path: 'todos',
-  component: TodosPageComponent
-}]
+const routes: Routes = [
+  {
+    path: 'todos',
+    title: 'Todos Page',
+    providers: [
+      {
+        provide: 'Angular',
+        useValue: 'v14',
+      },
+    ],
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./app/todos-page/todos-page.component').then((m) => m.TodosPageComponent),
+      },
+    ],
+  },
+];
 
 bootstrapApplication(AppComponent, {
   providers: [
